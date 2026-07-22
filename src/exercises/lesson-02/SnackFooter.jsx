f5778520f71a6374a4a9c9953edb1520627bd06f
Your Name <youremail@example.com>
2026-07-21 22:18:01 -0700

    Complete lesson 2 components exercise

---
+++ src/exercises/lesson-02/SnackFooter.jsx
@@ -0,0 +1,8 @@
+function SnackFooter() {
+  return (
+    <footer>
+      <p>Yummy yummy in my tummy 2026!</p>
+    </footer>
+  );
+}
+export default SnackFooter;
---
+++ src/exercises/lesson-02/SnackHeader.jsx
@@ -0,0 +1,5 @@
+function SnackHeader() {
+  return <h1>My Favorite Snacks</h1>;
+}
+
+export default SnackHeader;
